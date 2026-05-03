07eff44cc3589634bcc57f525f37c9d3a4914570
pigarcia12 <pigarcia@csumb.edu>
2026-05-02 23:51:35 -0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2761
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\froman\fcharset0 Times-Roman;}
+{\colortbl;\red255\green255\blue255;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs24 \cf0 \expnd0\expndtw0\kerning0
+\outl0\strokewidth0 \strokec2 import streamlit as st\
+import qrcode\
+from PIL import Image\
+\
+data = st.text_input("Enter text or URL", "Python is fun")\
+\
+img = qrcode.make(data)\
+img.save('MyQRCode1.jpg')\
+\
+img = Image.open("MyQRCode1.jpg")\
+st.image(img)}
